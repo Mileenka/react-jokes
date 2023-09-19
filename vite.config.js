6fb3400cbed6f6524bf44c84be847bf01d38beb3
Milena Wasowicz <milena.wasowicz@gmail.com>
2023-09-19 13:38:46 +0200

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/todo-list",
+  base: "/react-jokes",
   build: {
     chunkSizeWarningLimit: 1600,
   },
